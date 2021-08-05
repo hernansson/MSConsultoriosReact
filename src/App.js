71a4bdc68d1278a4efa4326a40e66d2f56834411
Hernan <hernan.ss@live.com>
2021-08-05 02:30:19 -0300

    Agregue itemList par ausarlo como filtro simple, logica de stock consistente en toda la pgina de manera local.

--- src/App.js
+++ src/App.js
@@ -4,27 +4,27 @@ import NavBar from './components/NavBar/NavBar.jsx';
 import Home from './components/pages/Home/Home'
 import Productos from './components/pages/Productos/Productos';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Categories from './components/pages/Productos/Categories/Categories';
-import ItemDetailContainer from './components/pages/Productos/ItemListContainer/ItemDetailContainer/ItemDetailContainer';
+import ItemDetailContainer from './components/pages/Productos/ItemListContainer/ItemList/Item/ItemDetailContainer/ItemDetailContainer'
 import CustomProvider from './components/Context/CustomProvider';
 import Cart from './components/pages/Cart/Cart';
 import "tailwindcss/tailwind.css"
+import ItemListContainer from './components/pages/Productos/ItemListContainer/ItemListContainer';
 
 function App() {
   return (
 
     <BrowserRouter>
-    <CustomProvider>
-      <NavBar />
-      <div >
-        <Switch>
-          <Route exact path={'/'} exact component={Home} />
-          <Route exact path={'/Productos'} exact component={Productos} />
-          <Route path={'/Productos/Categoria/:categoriaID'} exact component={Categories} />
-          <Route path={'/Detalle/:title/:iD'} exact component={ItemDetailContainer} />
-          <Route exact path={'/Cart'} exact component={Cart} />        
-        </Switch>
-      </div>
+      <CustomProvider>
+        <NavBar />
+        <div >
+          <Switch>
+            <Route exact path={'/'} exact component={Home} />
+            <Route exact path={'/Productos'} exact component={Productos} />
+            <Route path={'/Productos/Categoria/:id'} exact component={ItemListContainer} />
+            <Route path={'/Detalle/:title/:iD'} exact component={ItemDetailContainer} />
+            <Route exact path={'/Cart'} exact component={Cart} />
+          </Switch>
+        </div>
       </CustomProvider>
     </BrowserRouter>
 
